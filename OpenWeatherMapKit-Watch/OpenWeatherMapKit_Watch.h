c6617ee76a191c3fafb64c428f39a30fbb865cd4
Anver Bogatov <anver.bogatoff@gmail.com>
2018-04-07 16:15:03 +0400

    WatchOs target was added to the project

---
+++ OpenWeatherMapKit-Watch/OpenWeatherMapKit_Watch.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherMapKit_Watch.h
+//  OpenWeatherMapKit-Watch
+//
+//  Created by Anver Bogatov on 07/04/2018.
+//  Copyright © 2018 Anver Bogatov. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for OpenWeatherMapKit_Watch.
+FOUNDATION_EXPORT double OpenWeatherMapKit_WatchVersionNumber;
+
+//! Project version string for OpenWeatherMapKit_Watch.
+FOUNDATION_EXPORT const unsigned char OpenWeatherMapKit_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherMapKit_Watch/PublicHeader.h>
+
+
